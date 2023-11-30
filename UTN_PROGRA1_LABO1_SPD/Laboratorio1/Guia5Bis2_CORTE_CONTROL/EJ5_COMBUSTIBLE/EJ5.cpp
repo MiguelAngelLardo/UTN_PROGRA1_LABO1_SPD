60a50a8f97e147f6a192ad58013b1a18d825c202
MiguelAngelLardo <miguel.lardo@yahoo.com.ar>
2023-11-30 18:29:00 -0300

    ej5 guiaBis labo

--- UTN_PROGRA1_LABO1_SPD/Laboratorio1/Guia5Bis2_CORTE_CONTROL/EJ5_COMBUSTIBLE/EJ5.cpp
+++ UTN_PROGRA1_LABO1_SPD/Laboratorio1/Guia5Bis2_CORTE_CONTROL/EJ5_COMBUSTIBLE/EJ5.cpp
@@ -8,12 +8,20 @@ int main(){
 
   float maxLitros, maxTipoCombustible1, maxTipoCombustible2, maxTipoCombustible3, maxTipoCombustible4; //PTOA
   int tipoMax; //PTOA
+  float maxRecaudacion = 0, recaudacionSucursal; //PTOB 
+  int maxSucursal;//PTOB
+  float recaudPrimQuin = 0, recaudSecQuin = 0; //PTOC
+  float acumLitros1 = 0, acumLitros2 = 0, acumLitros3 = 0, acumLitros4 = 0; //PTOD
+  float ventaIndivMax; //PTOE
+  int tipoCombustibleMax; // PTOE
 
   cout << "Ingrese numero sucursal (1 al 15) (CORTA CON CERO):  "; 
   cin >> numSucursal;
   while(numSucursal !=0)
   {
     maxTipoCombustible1 = maxTipoCombustible2 = maxTipoCombustible3 = maxTipoCombustible4 = 0.0; //PTOA
+    recaudacionSucursal = 0; //PTOB
+    ventaIndivMax = 0; //PTOE
 
     numSucAnterior = numSucursal;
     while (numSucursal == numSucAnterior)
@@ -26,29 +34,41 @@ int main(){
       cin >> tipoCombustible;
       cout << "Importe de la venta ($): "; 
       cin >> importe;
-      cout << "Libros vendidos: "; 
+      cout << "Litros vendidos: "; 
       cin >> litrosVendidos;
 
       switch (tipoCombustible)
       {
       case 1:
         maxTipoCombustible1 += litrosVendidos;
+        acumLitros1 += (tipoDia == 1) ? litrosVendidos : 0;
         break;
       case 2:
          maxTipoCombustible2 += litrosVendidos;
+         acumLitros2 += (tipoDia == 1) ? litrosVendidos : 0;
          break;
       case 3:
          maxTipoCombustible3 += litrosVendidos;
+         acumLitros3 += (tipoDia == 1) ? litrosVendidos : 0;
          break;
       case 4:
          maxTipoCombustible4 += litrosVendidos;
+         acumLitros4 += (tipoDia == 1) ? litrosVendidos : 0;
          break;       
       }
 
+      recaudacionSucursal += litrosVendidos * importe; //PTOB
 
+      recaudPrimQuin += (dia >=1 && dia <= 15) ? recaudacionSucursal : 0;//PTOC
+      recaudSecQuin += (dia >=16 && dia <= 30) ? recaudacionSucursal : 0;//PTOC
 
-     cout << "Ingrese numero sucursal (1 al 15) (CORTA CON CERO):  "; 
-     cin >> numSucursal;
+      if(importe * litrosVendidos  > ventaIndivMax){
+        ventaIndivMax = importe * litrosVendidos;
+        tipoCombustibleMax = tipoCombustible;
+      }
+
+      cout << "Ingrese numero sucursal (1 al 15) (CORTA CON CERO):  "; 
+      cin >> numSucursal;
     }
 
     if(maxTipoCombustible1 > maxTipoCombustible2 && maxTipoCombustible1 > maxTipoCombustible3 && maxTipoCombustible1 > maxTipoCombustible4){
@@ -74,19 +94,20 @@ int main(){
       cout << "comun ";
     }else if(tipoMax == 4){
       cout << "gasoil ";
-    }    
-    
+    }        
     cout << "cantidad de litros: " << maxLitros << endl;
 
+    cout << "PUNTO E: " << "SUCURSAL: " << numSucAnterior << " - venta individual mayor: $" << ventaIndivMax << " - TIPO COMBUSTIBLE: " << tipoCombustibleMax << endl;
 
-
-
-
+    if(recaudacionSucursal > maxRecaudacion){
+      maxRecaudacion = recaudacionSucursal;
+      maxSucursal = numSucAnterior;
+    }
   }  
 
-
-  
-
+  cout << endl << "PUNTO B: Maxima recaudacion en sucursal: " << maxSucursal << " es : $" << maxRecaudacion << endl;
+  cout << "PUNTO C: RECUADACION PRIMER QUINCENA: " << recaudPrimQuin << " - RECAUDACION SEGUNDA QUINCENA: " << recaudSecQuin << endl;
+  cout << "PUNTO D: LITROS VENDIDOS EXTRA: " << acumLitros1 << " - LITROS VENDIDOS SUPER: " << acumLitros2 << " - LITROS VENDIDOS COMUN: " << acumLitros3 << " - LITROS VENDIDOS GASOIL: " << acumLitros4 << endl;
 
   return 0;
 }
